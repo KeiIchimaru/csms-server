7d2ffe0a3422589f4eaf63b56f66882f33e24f51
Kei Ichimaru <kei.ichimaru.3776.24@gmail.com>
2020-03-16 18:42:16 +0900

    update

--- models/tournament/Management.js
+++ models/tournament/Management.js
@@ -16,7 +16,7 @@ const {
 
 async function _getParticipatingPlayerEventOrder(conn, day, gender, classification, event) {
   let sql = 'SELECT * FROM viewParticipatingPlayerEventOrder where s_acting_day = ? and s_gender = ? and s_classification = ? and a_event_id = ?';
-  sql += ' order by s_number ASC, a_sequence ASC, c_number ASC, p_sequence ASC';
+  sql += ' order by s_number ASC, a_sequence ASC, c_number ASC, p_sequence ASC, p_bibs ASC';
   let params = [ day, gender, classification, event ];
   const results = await conn.query(sql, params);
   return results;
